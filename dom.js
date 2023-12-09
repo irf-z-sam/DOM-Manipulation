5d93a58ea71ccef9d46e413ec26cd40b363573a8
irf-z-sam <irfansamsi555@gmail.com>
2023-12-09 17:49:14 +0530

    selectByTagName

--- dom.js
+++ dom.js
@@ -1,6 +1,5 @@
-var items=document.getElementsByClassName("list-group-item");
-console.log(items);
-items[2].style.backgroundColor="green";
-for(let i=0;i<items.length;i++){
-    items[i].style.fontWeight="bold";  
-}
+var li=document.getElementsByClassName("list-group-item");
+console.log(li);
+
+var li=document.getElementsByTagName("li");
+console.log(li);
